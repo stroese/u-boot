358c2ab8aadaae5043dcc094f11ffd5abd5ebb7f
Stefan Roese <sr@denx.de>
2019-04-03 12:45:38 +0200

    WIP: drivers/watchdog/wdt-uclass.c
    Signed-off-by: Stefan Roese <sr@denx.de>

--- drivers/watchdog/wdt-uclass.c
+++ drivers/watchdog/wdt-uclass.c
@@ -10,6 +10,12 @@
 #include <dm/device-internal.h>
 #include <dm/lists.h>
 
+#ifndef WDT_DEFAULT_TIMEOUT
+#define WDT_DEFAULT_TIMEOUT	60
+#endif
+
+DECLARE_GLOBAL_DATA_PTR;
+
 int wdt_start(struct udevice *dev, u64 timeout_ms, ulong flags)
 {
 	const struct wdt_ops *ops = device_get_ops(dev);
@@ -63,6 +69,67 @@ int wdt_expire_now(struct udevice *dev, ulong flags)
 	return ret;
 }
 
+#if defined(CONFIG_WATCHDOG)
+/*
+ * Called by macro WATCHDOG_RESET. This function be called *very* early,
+ * so we need to make sure, that the watchdog driver is ready before using
+ * it in this function.
+ */
+void watchdog_reset(void)
+{
+	static ulong next_reset;
+	ulong now;
+
+	/* Exit if GD is not ready or watchdog is not initialized yet */
+	if (!gd || !(gd->flags & GD_FLG_WDT_READY))
+		return;
+
+	/* Do not reset the watchdog too often */
+	now = get_timer(0);
+	if (now > next_reset) {
+		next_reset = now + 1000;	/* reset every 1000ms */
+		wdt_reset(gd->watchdog_dev);
+	}
+}
+
+static void watchdog_start(void)
+{
+	u32 timeout = WDT_DEFAULT_TIMEOUT;
+
+	/*
+	 * Use only the first watchdog device in U-Boot to trigger the
+	 * watchdog reset
+	 */
+	if (gd->watchdog_dev) {
+		debug("Only one watchdog device used!\n");
+		return;
+	}
+
+	/*
+	 * Init watchdog: This will call the probe function of the
+	 * watchdog driver, enabling the use of the device
+	 */
+	if (uclass_get_device(UCLASS_WDT, 0,
+			      (struct udevice **)&gd->watchdog_dev)) {
+		debug("Watchdog: Not found!\n");
+		return;
+	}
+
+	if (CONFIG_IS_ENABLED(OF_CONTROL)) {
+		timeout = dev_read_u32_default(gd->watchdog_dev, "timeout-sec",
+					       WDT_DEFAULT_TIMEOUT);
+	}
+
+	wdt_start(gd->watchdog_dev, timeout * 1000, 0);
+	gd->flags |= GD_FLG_WDT_READY;
+	printf("WDT:   Started (%ds timeout)\n", timeout);
+}
+#else
+static void watchdog_start(void)
+{
+}
+#endif
+
 static int wdt_post_bind(struct udevice *dev)
 {
 #if defined(CONFIG_NEEDS_MANUAL_RELOC)
@@ -82,6 +149,13 @@ static int wdt_post_bind(struct udevice *dev)
 		reloc_done++;
 	}
 #endif
+
+	/*
+	 * Start the watchdog service in U-Boot if selected via
+	 * CONFIG_WATCHDOG
+	 */
+	watchdog_start();
+
 	return 0;
 }
 
